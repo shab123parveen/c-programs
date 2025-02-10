b31603adbd196da44c7cde4d47b24b796f1d6d4c
shab123parveen <shab123parveen@gmail.com>
2025-02-10 22:38:12 +0530

    changed in main.c file

--- Workspace1/favouriteNumber/main.c
+++ Workspace1/favouriteNumber/main.c
@@ -5,6 +5,6 @@ int main(int argc, char **argv)
     int myNumber;
 	printf("Enter your favourite number\n");
     scanf("%d",&myNumber);
-    printf("Your favourite number is %d\n",myNumber);
+    printf("Your entered favourite number is %d\n",myNumber);
 	return 0;
 }
