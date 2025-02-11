0a778929bd8020d104a70832fc5449a317feaba7
shab123parveen <shab123parveen@gmail.com>
2025-02-11 12:39:13 +0530

    added thank you msg at the end

--- Workspace1/favouriteNumber/main.c
+++ Workspace1/favouriteNumber/main.c
@@ -6,5 +6,6 @@ int main(int argc, char **argv)
 	printf("Enter Your Favourite Number\n");
     scanf("%d",&myNumber);
     printf("Your Entered Favourite Number Is %d\n",myNumber);
+    printf("Thank You For Using This Program\n");
 	return 0;
 }
